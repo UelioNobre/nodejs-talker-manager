8a10bd85937e9a29b16b49af7bc393b5aa634785
uelionobre <uelionobre@hotmail.com>
2023-03-22 09:28:11 -0300

    Implementa as rotas para talkers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const talkerRouter = require('./routes/talker.routes');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+
+app.use('/talker', talkerRouter);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
