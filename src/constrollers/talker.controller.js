4ef64fb79b550f8a63020154c77ca93d333cab15
uelionobre <uelionobre@hotmail.com>
2023-03-22 09:26:44 -0300

    Cria controller de talkers

---
+++ src/constrollers/talker.controller.js
@@ -0,0 +1,18 @@
+const { readTalkers } = require('../utils/fsTalkers');
+
+const getAll = async (_, res) => {
+  const talkersData = await readTalkers();
+  res.status(200).json(talkersData);
+};
+
+const getTalkerById = async (req, res) => {
+  const { id } = req.params;
+  const talkersData = await readTalkers();
+  const talker = talkersData.find((talk) => talk.id === +id);
+  return res.status(200).json(talker);
+}
+
+module.exports = {
+  getAll,
+  getTalkerById
+}
