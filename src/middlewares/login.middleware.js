4f7c183c848cad6581bddc254f1593ee1afa53be
uelionobre <uelionobre@hotmail.com>
2023-03-22 11:01:04 -0300

    Cria o middlware de Login

---
+++ src/middlewares/login.middleware.js
@@ -0,0 +1,25 @@
+const validateEmail = require('../utils/validateEmail');
+
+const validateInputs = (req, res, next) => {
+  const { body } = req;
+  let message = 'O campo "email" é obrigatório';
+
+  if (!Object.keys(body).includes('email')) return res.status(400).json({ message });
+
+  const { email } = req.body;
+  message = 'O "email" deve ter o formato "email@email.com"';
+  if (!validateEmail(email)) return res.status(400).json({ message });
+  
+  message = 'O campo "password" é obrigatório';
+  if (!Object.keys(body).includes('password')) return res.status(400).json({ message });
+  
+  const { password } = req.body;
+  message = 'O "password" deve ter pelo menos 6 caracteres';
+  if (password.length < 6) return res.status(400).json({ message });
+
+  next();
+};
+
+module.exports = {
+  validateInputs,
+};
