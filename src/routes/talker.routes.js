b8ef1e6ad6e57ad8c0b9f38d143591c2244cd4b4
uelionobre <uelionobre@hotmail.com>
2023-03-26 12:29:20 -0300

    Adiciona rota POST em no endpoint /talker

--- src/routes/talker.routes.js
+++ src/routes/talker.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { talkerExists } = require('../middlewares/talkers.middlware');
+const { talkerExists, talkerCreateValidation } = require('../middlewares/talkers.middlware');
 const talkerController = require('../constrollers/talker.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 const talkerRouter = Router();
 
 talkerRouter.get('/', talkerController.getAll);
 talkerRouter.get('/:id', talkerExists, talkerController.getTalkerById);
+talkerRouter.post('/', authMiddleware, talkerCreateValidation, talkerController.addTalker);
 
 module.exports = talkerRouter;
