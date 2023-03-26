97661ee03955766cb1f8cda7e285cc6d3da3f252
uelionobre <uelionobre@hotmail.com>
2023-03-26 12:30:36 -0300

    Adiciona validacao para criacao de um talker

--- src/middlewares/talkers.middlware.js
+++ src/middlewares/talkers.middlware.js
@@ -1,4 +1,7 @@
 const { readTalkers } = require('../utils/fsTalkers');
+const { validateDateFormat, validateRate } = require('../utils/validations');
+
+
 
 const talkerExists = async (req, res, next) => {
   const talkersData = await readTalkers();
@@ -12,6 +15,50 @@ const talkerExists = async (req, res, next) => {
   next();
 };
 
+const talkerCreateValidation = (req, res, next) => {
+  const { name, age, talk } = req.body;
+
+  if (!name) return res.status(400).json({
+    message: 'O campo "name" é obrigatório'
+  });
+  
+  if (name.length < 3) return res.status(400).json({
+    message: 'O "name" deve ter pelo menos 3 caracteres'
+  });
+  
+  if (!age) return res.status(400).json({
+    message: 'O campo "age" é obrigatório'
+  });
+  
+  if (!Number.isInteger(age) || age < 18) return res.status(400).json({
+    message: 'O campo "age" deve ser um número inteiro igual ou maior que 18'
+  });
+
+  if (!talk) return res.status(400).json({
+    message: 'O campo "talk" é obrigatório'
+  });
+
+  if (!Object.keys(talk).includes('rate')) return res.status(400).json({
+    message: 'O campo "rate" é obrigatório'
+  });
+
+  if (!validateRate(talk.rate)) return res.status(400).json({
+    message: 'O campo "rate" deve ser um número inteiro entre 1 e 5'
+  });
+
+  if (!Object.keys(talk).includes('watchedAt')) return res.status(400).json({
+    message: 'O campo "watchedAt" é obrigatório'
+  });
+
+  if (!validateDateFormat(talk.watchedAt)) return res.status(400).json({
+    message: "O campo \"watchedAt\" deve ter o formato \"dd/mm/aaaa\""
+  });
+
+
+  next();
+};
+
 module.exports = {
   talkerExists,
+  talkerCreateValidation
 };
