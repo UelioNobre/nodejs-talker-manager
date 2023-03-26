cd96812b46bff29afcfb815185cca7e47cea469b
uelionobre <uelionobre@hotmail.com>
2023-03-26 12:31:21 -0300

    Adiciona um novo talker

--- src/constrollers/talker.controller.js
+++ src/constrollers/talker.controller.js
@@ -1,4 +1,4 @@
-const { readTalkers } = require('../utils/fsTalkers');
+const { readTalkers, writeTalkers } = require('../utils/fsTalkers');
 
 const getAll = async (_, res) => {
   const talkersData = await readTalkers();
@@ -12,7 +12,23 @@ const getTalkerById = async (req, res) => {
   return res.status(200).json(talker);
 };
 
+const addTalker = async (req, res) => {
+
+  // Controller
+  const talker = req.body;
+
+  // Service
+  const talkers = await readTalkers();
+  const id = talkers.length + 1;
+  const newTalker = { ...talker, id };
+  await writeTalkers(newTalker);
+
+  // Controller
+  return res.status(201).json(newTalker);
+}
+
 module.exports = {
   getAll,
   getTalkerById,
+  addTalker
 };
