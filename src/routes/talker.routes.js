f10c4747494e67b676c85cef561f1ebfb4ef3a5a
uelionobre <uelionobre@hotmail.com>
2023-03-26 21:47:25 -0300

    Adiciona a rota para atualizar talkers

--- src/routes/talker.routes.js
+++ src/routes/talker.routes.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
-const { talkerExists, talkerCreateValidation } = require('../middlewares/talkers.middlware');
-const talkerController = require('../constrollers/talker.controller');
+const {
+  talkerExists,
+  talkerCreateValidation,
+  talkerUpdateMiddleware,
+} = require('../middlewares/talkers.middlware');
+
 const authMiddleware = require('../middlewares/auth.middleware');
+const talkerController = require('../constrollers/talker.controller');
 
 const talkerRouter = Router();
 
 talkerRouter.get('/', talkerController.getAll);
 talkerRouter.get('/:id', talkerExists, talkerController.getTalkerById);
 talkerRouter.post('/', authMiddleware, talkerCreateValidation, talkerController.addTalker);
+
+talkerRouter.put('/:id',
+  authMiddleware,
+  talkerUpdateMiddleware,
+  talkerController.updateTalker);
 
 module.exports = talkerRouter;
