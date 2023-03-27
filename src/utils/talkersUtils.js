97b94dd2e0a4f1b6ab913494a11ce005cf46f54a
uelionobre <uelionobre@hotmail.com>
2023-03-27 16:20:19 -0300

    Torna a funcao de pesquisar palestrante pelo nome como case-insensitive

--- src/utils/talkersUtils.js
+++ src/utils/talkersUtils.js
@@ -1,5 +1,5 @@
 const findTalkerByName = (term, payload) => payload
-    .filter(({ name }) => name.includes(term));
+  .filter(({ name }) => name.toLowerCase().includes(term.toLowerCase()));
 
 module.exports = {
   findTalkerByName,
