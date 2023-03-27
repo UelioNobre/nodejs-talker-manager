d8826f2afced36e702a954779e69aa0d10b11c12
uelionobre <uelionobre@hotmail.com>
2023-03-26 21:45:37 -0300

    Cria o middleware para validar dados de atuaizacao

--- src/middlewares/talkers.middlware.js
+++ src/middlewares/talkers.middlware.js
@@ -29,7 +29,29 @@ const talkerCreateValidation = (req, res, next) => {
   next();
 };
 
+const talkerUpdateMiddleware = async (req, res, next) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  const validateName = isValidName(name);
+  if (validateName.type) return requestErrors(res, validateName.type);
+
+  const validateAge = isValidAge(age);
+  if (validateAge.type) return requestErrors(res, validateAge.type);
+
+  const validateTalk = isTalkValid(talk);
+  if (validateTalk.type) return requestErrors(res, validateTalk.type);
+
+  const talkersData = await readTalkers();
+  const isExists = talkersData.some((t) => t.id === +id);
+  if (!isExists) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  next();
+};
+
 module.exports = {
   talkerExists,
   talkerCreateValidation,
+  talkerUpdateMiddleware,
 };
