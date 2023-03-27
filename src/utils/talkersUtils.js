8d227a1be616d5072b0e12deaf46252369d9a9e8
uelionobre <uelionobre@hotmail.com>
2023-03-27 18:02:15 -0300

    Adiciona a funcao para filtar palestrantes pelo Rate

--- src/utils/talkersUtils.js
+++ src/utils/talkersUtils.js
@@ -1,6 +1,9 @@
 const findTalkerByName = (term, payload) => payload
   .filter(({ name }) => name.toLowerCase().includes(term.toLowerCase()));
 
+const findTalkerByRate = (rate, payload) => payload.filter(({ talk }) => talk.rate === rate);
+
 module.exports = {
   findTalkerByName,
+  findTalkerByRate,
 };
