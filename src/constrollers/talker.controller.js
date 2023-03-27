d7d4d971027702ae0cb295882875739c91784cd2
uelionobre <uelionobre@hotmail.com>
2023-03-26 21:48:25 -0300

    Cria o metodo para atualizar talkers no controller de talkers

--- src/constrollers/talker.controller.js
+++ src/constrollers/talker.controller.js
@@ -1,4 +1,4 @@
-const { readTalkers, writeTalkers } = require('../utils/fsTalkers');
+const { readTalkers, writeTalkers, updateTalkers } = require('../utils/fsTalkers');
 
 const getAll = async (_, res) => {
   const talkersData = await readTalkers();
@@ -26,8 +26,29 @@ const addTalker = async (req, res) => {
   return res.status(201).json(newTalker);
 };
 
+const updateTalker = async (req, res) => {
+  // Controller
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+
+  // Service
+  const talkersData = await readTalkers();
+  const talker = { ...talkersData.find((t) => t.id === +id), name, age, talk };
+
+  const talkers = talkersData.map((t) => {
+    if (t.id === +id) return talker;
+    return talk;
+  });
+
+  await updateTalkers(talkers);
+
+  // Controller
+  return res.status(200).json({ ...talker });
+};
+
 module.exports = {
   getAll,
   getTalkerById,
   addTalker,
+  updateTalker,
 };
